d8b56ea58c0dd2d66d814c90ff1776e5e9313872
SkinnyPigeon <philtheskinnypigeon@yahoo.co.uk>
2016-09-13 08:53:08 +0100

    initial commit

---
+++ app/controllers/shows_controller.rb
@@ -0,0 +1,25 @@
+class ShowsController < ApplicationController
+
+  respond_to :json
+
+  def index
+    @shows = Show.all
+    render :json => @shows
+  end 
+
+  def show
+    @show = Show.find( params[ :id ])
+    render :json => @show
+  end
+
+  def create
+    show = Show.create( show_params )
+    render :json => show
+  end
+
+  private
+  def show_params
+    params.require( :show ).permit( [:title, :series, :description, :image, :programmeID ] )
+  end
+
+end
